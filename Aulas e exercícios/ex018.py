adade9bf65dcd517f02d58989a063665bfeaac39
FelipeKauanG <felipekauanprofissional@gmail.com>
2023-09-26 15:31:09 -0300

    scikit-Learn

--- Aulas e exercícios/ex018.py
+++ Aulas e exercícios/ex018.py
@@ -1,4 +1,4 @@
-#faca um programa que leia um angulo qualquer, e mostre na tela o valor do seno cosseno e tangente.
+# faca um programa que leia um angulo qualquer, e mostre na tela o valor do seno cosseno e tangente.
 import math
 from math import sin, cos, tan
 
@@ -7,4 +7,4 @@ print("\033[0;32mSeno\033[m: {:.3f}\n"
       "\033[0;31mCosseno\033[m {:.3f}\n"
       "\033[0;36mTangente\033[m: {:.3f}".format(sin(math.radians(ang)), cos(math.radians(ang)), tan(math.radians(ang))))
 
-#converter em radianos
+# converter em radianos
--- Aulas e exercícios/ex111/__init__.py
+++
@@ -1,1 +0,0 @@
-from UtilidadesCeV import moeda, dado
