b43b9b693304276b363da27b49250aa7526c0fe0
FelipeKauanG <felipekauanprofissional@gmail.com>
2023-10-13 17:32:33 -0300

    v2.42

--- consulta de RA/verRAs.py
+++ consulta de RA/verRAs.py
@@ -1,48 +1,40 @@
-try:
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    import numpy as np
-    
-    # Dados do aluno
-    ra = int(input("Insira aqui seu RA: "))
-    data = str(input("Sua data de nascimentos '01/02/1234: '"))
-    digito = int(input("Seu dígito: "))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-    # configurando o Driver
-    driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 7)
-    pontos = 0
-    num = []
 
-    for ano in range(2015, 2022):
+ra = str(input("RA do aluno (123456789): ")).strip()
+data = str(input("Data de nascimento (01 02 2003): ")).strip()
+digito = str(input("Dígito verificador (um digito): ")).strip()
+data.replace(" ", "/").strip()
+iniAno = int(input("Qual idade você tinha no 5º ano ?"))
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 7)
+pontos = 0
+num = []
+for ano in range(int(data[5:])+iniAno, int(data[5:])+iniAno+7):
+    try:
         try:
-            try:
-                print(f"\033[34mAno de {ano}\033[m")
-                driver.get(
-                    f"https://sed.educacao.sp.gov.br/Boletim/GerarBoletimUnificadoExterno?nrRa={'000'+ra}&nrDigRa={digito}&dsUfRa=SP&dtNascimento={data}&nrAnoLetivo={000+ano}")
-                print("\n")
-                
-                    
-                for notn in range(1, 13): # localiza as matérias
-                    for bim in range(2, 15, 4): # localiza os bimestres
-                        disciplinas = wait.until(EC.presence_of_all_elements_located((By.XPATH, f"/html/body/div/div/div[4]/table/tbody/tr[{notn}]/td[1]")))[0].text
-                        bimnot = wait.until(EC.presence_of_all_elements_located(
-                            (By.XPATH, f'/html/body/div/div/div[4]/table/tbody/tr[{notn}]/td[{bim}]')))[0].text
-                        cleaned_bimnot = str(bimnot).replace(" ", "").replace("-","")
-                        if cleaned_bimnot.isdigit():
-                            pontos += int(bimnot)
-                            num.append(bimnot)
-                    #print()
-            except Exception:
-                print("\n\n\n")
-        except Exception:
-            print(f"Não consegui ver este boletim!")
-    driver.quit()
-    num = np.array(num).reshape(len(num/4),4)
-    print(f"Pontos totais: {pontos}\nMédia total do aluno: \033[36m{pontos / len(num):.2f}\033[m")
-    print(num)
-except:
-    print("bom dia")
-    
+            print(f"Ano de \033[34m{ano}\033[m")
+            driver.get(
+                f"https://sed.educacao.sp.gov.br/Boletim/GerarBoletimUnificadoExterno?nrRa={'000'+ra}&nrDigRa={digito}&dsUfRa=SP&dtNascimento={data}&nrAnoLetivo={000+ano}"),
+            print("\n")
+ 
+            for notn in range(1, 13):
+                for bim in range(2, 15, 4):
+                    bimnot = wait.until(EC.presence_of_all_elements_located(
+                    (By.XPATH, f'/html/body/div/div/div[4]/table/tbody/tr[{notn}]/td[{bim}]')))[0].text
+                    if bimnot.isdigit:
+                        pontos += int(bimnot)
+                        num.append(bimnot)
+                        print(f"{bimnot}", end=" ")
+
+                print()
+        except:
+            print("\n\n\n")
+    except:
+        print(f"\033[34m\033Não consegui ver este boletim![m")
+driver.quit()
+print(f"\033[33mPontos totais {pontos}\033[m"
+      f"\n\033[32mMédia do aluno total: {pontos/int(num.count()):.2f}")
