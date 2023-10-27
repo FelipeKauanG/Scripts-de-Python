f2c3f12acad1ced0355672577b53327a413c8ece
FelipeKauanG <felipekauanprofissional@gmail.com>
2023-10-27 19:16:06 -0300

    v2.8

--- PixelArtMinecraft/script.py
+++ PixelArtMinecraft/script.py
@@ -1,8 +1,9 @@
-import PySimpleGUI as sg
-import numpy as np
-import matplotlib.pyplot as plt
-from time import sleep
-colors = {"Grass": "127, 178, 56", "Sand":"247, 233, 163", "Wool": "199, 199, 199", "Fire": "255, 0, 0", "Ice": "160, 160, 255", "Metal": "167, 167, 167", "Plant": "0, 124, 0", "Snow": "255, 255, 255", "Clay": "164, 168, 184", "Dirt": "151, 109, 77", "Stone": "112, 112, 112", "Water": "64, 64, 255", "Wood": "143, 119, 72", "Quartz": "255, 252, 245", "Color_orange": "216, 127, 51", "Color_magenta" : "178, 76, 216", "Color_light_blue": "102, 153, 216", "Color_yellow": "229, 229, 51", "Color_light_green": "127, 229, 51", "Color_pink": "242, 127, 165", "Color_gray": "76, 76, 76", "Color_light_gray": "153, 153, 153", "Color_cyan": "76, 127, 153", "Color_purple": "127, 63, 178", "Color_blue": "51, 76, 178", "Color_brown": "102, 76, 51", "Color_green": "102, 127, 51", "Color_red": "153, 51, 51", "Color_black": "25, 25, 25", "Gold":"250, 238, 25", "Diamond": "92, 219, 213", "Lapis": "74, 128, 255", "Emerald": "0, 217, 58", "Podzol": "129, 86, 49", "Nether": "112, 2, 0", "Terracotta_white": "209, 177, 161", "Terracotta_orange": "159, 872, 36", "Terracotta_magenta": "149, 87, 108", "Terracotta_light_blue": "112, 108, 138", "Terracotta_yellow": "186, 133, 36", "Terracotta_light_green": "103, 117, 53", "Terracotta_pink": "160, 77, 78", "Terracotta_gray": "57, 41, 35", "Teracotta_light_gray": "137, 107, 98","Terracotta_cyan": "87, 92, 92", "Terracotta_purple": "122, 73, 88", "Terracotta_blue": "76, 62, 92", "Terracotta_brown": "76, 50, 35","Terracotta_green": "76, 82, 42", "Terracotta_red": "142, 60, 46", "Terracotta_black": "37, 22, 16","Crimson_nylium":"189, 48, 49","Crimson_hyphae":"92, 25, 29","Crimsom_stem":"148, 63, 97", "Warped_nylium" : "22, 126, 134","Warped_stem":"58, 142, 140","Warped_Hyphae":"86, 44, 62","Warped_wart_block":"20, 180, 133","Deepslate":"100, 100, 100","Raw_iron":"216, 175, 147", "Glow_lichen":"127, 167, 150"}
+import PySimpleGUI as sg #pip install PySimpleGUI
+import numpy as np #pip install numpy
+import matplotlib.pyplot as plt # pip install matplotlib
+import os
+
+colors = {"Grass": "127, 178, 56", "Sand":"247, 233, 163", "Wool": "199, 199, 199", "Redstone_block": "255, 0, 0", "Ice": "160, 160, 255", "Metal": "167, 167, 167", "Plant": "0, 124, 0", "Snow": "255, 255, 255", "Clay": "164, 168, 184", "Dirt": "151, 109, 77", "Stone": "112, 112, 112", "Water": "64, 64, 255", "Wood": "143, 119, 72", "Quartz": "255, 252, 245", "Color_orange": "216, 127, 51", "Color_magenta" : "178, 76, 216", "Color_light_blue": "102, 153, 216", "Color_yellow": "229, 229, 51", "Color_light_green": "127, 229, 51", "Color_pink": "242, 127, 165", "Color_gray": "76, 76, 76", "Color_light_gray": "153, 153, 153", "Color_cyan": "76, 127, 153", "Color_purple": "127, 63, 178", "Color_blue": "51, 76, 178", "Color_brown": "102, 76, 51", "Color_green": "102, 127, 51", "Color_red": "153, 51, 51", "Color_black": "25, 25, 25", "Gold":"250, 238, 25", "Diamond": "92, 219, 213", "Lapis": "74, 128, 255", "Emerald": "0, 217, 58", "Spruce": "129, 86, 49", "Nether": "112, 2, 0", "Terracotta_white": "209, 177, 161", "Terracotta_orange": "159, 872, 36", "Terracotta_magenta": "149, 87, 108", "Terracotta_light_blue": "112, 108, 138", "Terracotta_yellow": "186, 133, 36", "Terracotta_light_green": "103, 117, 53", "Terracotta_pink": "160, 77, 78", "Terracotta_gray": "57, 41, 35", "Terracotta_light_gray": "137, 107, 98","Terracotta_cyan": "87, 92, 92", "Terracotta_purple": "122, 73, 88", "Terracotta_blue": "76, 62, 92", "Terracotta_brown": "76, 50, 35","Terracotta_green": "76, 82, 42", "Terracotta_red": "142, 60, 46", "Terracotta_black": "37, 22, 16","Crimson_nylium":"189, 48, 49","Crimson_hyphae":"92, 25, 29","Crimsom_stem":"148, 63, 97", "Warped_nylium" : "22, 126, 134","Warped_stem":"58, 142, 140","Warped_hyphae":"86, 44, 62","Warped_wart_block":"20, 180, 133","Deepslate":"100, 100, 100","Raw_iron":"216, 175, 147", "Glow_lichen":"127, 167, 150"}
 
 porcentagens = {}
 
@@ -46,12 +47,16 @@ def open_image():
                                     menor_diferença = diferenca
                                     melhor_cor = (valor_rgb, nome)
                                     str(melhor_cor).split(", ")
-                            textoImagem.write(f"#{melhor_cor[0][0]:02x}{melhor_cor[0][1]:02x}{melhor_cor[0][2]:02x}\n")
+                            textoImagem.write(f"#{melhor_cor[0][0]:02x}{melhor_cor[0][1]:02x}{melhor_cor[0][2]:02x}\n{melhor_cor[1]}\n")
     cores = []
+    blocks = r"PixelArtMinecraft/blocos"
+    block_itens = os.listdir(blocks)
+    
+    
     with open(arquivo, "r", encoding="utf-8") as textoImagem:
         
         lines = textoImagem.readlines()
-        for line in range(0, len(lines)):
+        for line in range(0, len(lines), 2):
             cor = lines[line].split()
             r = int(cor[0][1:3], 16) / 255
             g  = int(cor[0][3:5], 16) / 255
@@ -62,7 +67,8 @@ def open_image():
     cores = np.rot90(cores, k=3)
     plt.axis("off")
     plt.imshow(cores)
-    plt.show()  # Certifique-se de descomentar esta linha para exibir a imagem
+    #plt.show()
+    
 if __name__ == "__main__":
     open_image()
     
--- PixelArtMinecraft/teste.py
+++ PixelArtMinecraft/teste.py
@@ -1,24 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+
+colors = {"Grass": "127, 178, 56", "Sand":"247, 233, 163", "Wool": "199, 199, 199", "Redstone_block": "255, 0, 0", "Ice": "160, 160, 255", "Metal": "167, 167, 167", "Plant": "0, 124, 0", "Snow": "255, 255, 255", "Clay": "164, 168, 184", "Dirt": "151, 109, 77", "Stone": "112, 112, 112", "Water": "64, 64, 255", "Wood": "143, 119, 72", "Quartz": "255, 252, 245", "Color_orange": "216, 127, 51", "Color_magenta" : "178, 76, 216", "Color_light_blue": "102, 153, 216", "Color_yellow": "229, 229, 51", "Color_light_green": "127, 229, 51", "Color_pink": "242, 127, 165", "Color_gray": "76, 76, 76", "Color_light_gray": "153, 153, 153", "Color_cyan": "76, 127, 153", "Color_purple": "127, 63, 178", "Color_blue": "51, 76, 178", "Color_brown": "102, 76, 51", "Color_green": "102, 127, 51", "Color_red": "153, 51, 51", "Color_black": "25, 25, 25", "Gold":"250, 238, 25", "Diamond": "92, 219, 213", "Lapis": "74, 128, 255", "Emerald": "0, 217, 58", "Spruce": "129, 86, 49", "Nether": "112, 2, 0", "Terracotta_white": "209, 177, 161", "Terracotta_orange": "159, 872, 36", "Terracotta_magenta": "149, 87, 108", "Terracotta_light_blue": "112, 108, 138", "Terracotta_yellow": "186, 133, 36", "Terracotta_light_green": "103, 117, 53", "Terracotta_pink": "160, 77, 78", "Terracotta_gray": "57, 41, 35", "Terracotta_light_gray": "137, 107, 98","Terracotta_cyan": "87, 92, 92", "Terracotta_purple": "122, 73, 88", "Terracotta_blue": "76, 62, 92", "Terracotta_brown": "76, 50, 35","Terracotta_green": "76, 82, 42", "Terracotta_red": "142, 60, 46", "Terracotta_black": "37, 22, 16","Crimson_nylium":"189, 48, 49","Crimson_hyphae":"92, 25, 29","Crimsom_stem":"148, 63, 97", "Warped_nylium" : "22, 126, 134","Warped_stem":"58, 142, 140","Warped_hyphae":"86, 44, 62","Warped_wart_block":"20, 180, 133","Deepslate":"100, 100, 100","Raw_iron":"216, 175, 147", "Glow_lichen":"127, 167, 150"}
+
+ordened_names = sorted(colors.keys())
+ordened_colors = {chave: colors[chave] for chave in ordened_names}
 
 
-data = ["#646464",
-"#4c4c4c",
-"#392923",
-"#4c4c4c",
-"#575c5c",
-"#4c4c4c",
-"#646464",
-"#664c33",
-"#664c33",
-"#4c3223",
-"#191919"]
+pasta = r"PixelArtMinecraft/blocos"
 
-colors_rgb = [tuple(int(color[i:i+2], 16) / 255 for i in (1, 3, 5)) for color in data]
-print(colors_rgb)
-data = np.array(colors_rgb).reshape(1, len(data), 3)
+pasta_items = os.listdir(pasta)
 
-plt.imshow(data, cmap="viridis")
-plt.axis("off")
-plt.show()
-print(data)
+
+for k in colors.keys():
+    
+    if f"{k}.png" in pasta_items:
+        print(f'\033[32m{k} está correto!\033[m')
+    else:
+        print(f"\033[31m{k} não está correto\033[m")
+    
