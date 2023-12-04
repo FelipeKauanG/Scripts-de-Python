1ec6c338aeda92d4df1346a45190fcb03fa2bc16
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-12-04 16:51:47 -0300

    v3.1

--- PixelArtMinecraft/Script.py
+++ PixelArtMinecraft/Script.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
-import PySimpleGUIQt as sg
+import PySimpleGUI as sg
 import numpy as np
 import fractions
 
-layout =
+sg.theme("DarkAmber")
+
+layout = [[sg.Image("")][sg.FileBrowse("Procurar imagem")], [sg.Button("Cancel")]]
+
+window = sg.Window("Pixel Mosaic Minecraft", layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == "Cancel":
+        break
+    window.close()
