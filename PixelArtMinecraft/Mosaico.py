61e18f8d9d79d262f767b742420f1c1d55665000
Felipe Kauan <felipekauanprofissional@gmail.com>
2024-01-14 11:37:56 -0300

    v3.1

--- PixelArtMinecraft/Mosaico.py
+++ PixelArtMinecraft/Mosaico.py
@@ -1,7 +1,23 @@
+import PySimpleGUI as sg #pip install PySimpleGUIQ
+import numpy as np #pip install numpy
+from matplotlib import pyplot as plt # pip install matplotlib
+from time import sleep
 import os
+from tkinter import *
+from fractions import Fraction
 from PIL import Image
+
+
 pasta = os.listdir(r"PixelArtMinecraft\blocos")
 for bloco in pasta:
-    print(f'\033[34m{bloco}\033[m'.replace(".png",""))
+    imagem = Image.open(f"PixelArtMinecraft/blocos/{bloco}")
+    imagem = imagem.convert("RGB")
+    largura,altura = imagem.size
+    #print(f"Largura: {largura}x{altura}")
+    print(f"\033[32m{bloco}\033[m")
+    
+    
+
+
 
 print(len(pasta))
