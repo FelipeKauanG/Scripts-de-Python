ee04ae4654792c14a75cc60bf6f6b4b130e2ce14
FelipeKauanG <felipekauanprofissional@gmail.com>
2024-10-06 06:55:01 -0300

    v3.6

--- uteis/Games/GTA SA MODELS LOCATOR/script.py
+++ uteis/Games/GTA SA MODELS LOCATOR/script.py
@@ -1,40 +1,108 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
-
 import os
+import shutil
+
+from math import ceil
 
 
 xpath = "/html/body/div[6]/div/div[8]/div/div[5]/div/div/div[2]/div/div/input[2]//div"
 
-
-
-endereço = os.path.abspath(r"C:\Users\Felipe Kauan\Desktop\Object models near point 2712, -1740.html")
-
-
-driver = webdriver.Chrome()
-driver.get(endereço)
-
-className = "mp-item"
-
-elementos = driver.find_elements(By.CLASS_NAME,className)
-
 DFFs = []
 
+pastaDFF = r"C:\\Users\\Felipe Kauan\\3D Objects\\GTA SA ALL DFFs"
 
-for elemento in elementos:
-    nome = elemento.get_attribute("data-name")
-    DFFs.append(nome)
+pastaDestino = r"C:\\Users\\Felipe Kauan\\Desktop\\Nova pasta"
 
-print(f"Tem {len(elementos)} ao todo no site")
+def excluirArquivos():
+    pastaArquivo = os.listdir(pastaDestino)
 
-print(DFFs)
+    for arquivo in pastaArquivo:
+        file = f"{pastaDestino}\\{arquivo}"
+        shutil.rmtree(file)
+
+
+excluirArquivos()
+
+
+
+def pegarNomes():
+    endereço = os.path.abspath(r"C:\\Users\\Felipe Kauan\\Desktop\\Mapa.html")
+
+    driver = webdriver.Chrome()
+    driver.get(endereço)
+
+    className = "mp-item"
+
+    elementos = driver.find_elements(By.CLASS_NAME,className)
+
     
 
-#print(f"\033[34mTem {len()} itens\033[m")
 
-driver.quit()
+    for elemento in elementos:
+        nome = elemento.get_attribute("data-name")
+        DFFs.append(f"{nome}.dff")
 
+    print(f"Tem {len(elementos)} ao todo no site")
+
+    driver.quit()
+
+pegarNomes()
+
+
+
+quantPastas = (ceil(len(DFFs)/50))
+print(f"\033[96m{quantPastas} pastas\033[m")
+
+
+Lista = []
+def listarDffs():
+    for arquivo in DFFs:
+        caminho_arquivo = os.path.join(pastaDFF, arquivo)
+
+        if os.path.isfile(caminho_arquivo):
+            tamanho = os.path.getsize(caminho_arquivo)
+            arquivo = (arquivo, tamanho)
+            Lista.append(arquivo)
+
+listarDffs()
+
+listaNova = sorted(Lista, key=lambda x: x[1], reverse=True)
+
+
+pasta_origem = "C:\\Users\\Felipe Kauan\\3D Objects\\GTA SA ALL DFFs"
+
+pastaDestino = r"C:\\Users\\Felipe Kauan\\Desktop\\Nova pasta\\Parte"
+
+
+quantArquivos = ceil(len(DFFs) / quantPastas)
+
+def copiarDffs():
+    indexPasta = 1
+    atual = 0
+
+    for arquivo in range(0, len(listaNova)):
+        
+        indexPasta += 1
+        atual = 0
+
+        pastaAtual = f"{pastaDestino} {indexPasta}"
+
+        os.makedirs(pastaAtual, exist_ok=True)
+
+        caminho_origem = os.path.join(pasta_origem, arquivo[arquivo][0])
+
+        caminho_destino = os.path.join(pastaAtual, arquivo)
+
+        print(atual)
+
+        #shutil.copy(caminho_origem, caminho_destino)
+
+        if atual == quantArquivos:
+            atual = 0
+            indexPasta += 1
+
+        atual += 1
+        
+copiarDffs()
